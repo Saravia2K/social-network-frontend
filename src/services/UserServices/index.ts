7cef06149e2da3f64b68b77b237ee1a7a4208c03
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-03-04 10:06:51 -0600

    Using basicFetch helper

--- src/services/UserServices/index.ts
+++ src/services/UserServices/index.ts
@@ -1,58 +1,20 @@
 import { API_URL } from "../../utils/consts";
-import { TErrorResponse } from "../../utils/types";
-import { RegisterUserDTO, TLoginResponse, TUser } from "./types";
+import { basicFetch } from "../../utils/helpers";
+import { RegisterUserDTO, TUser } from "./types";
 
 export default class UserServices {
   static USERS_URL = `${API_URL}/usuarios`;
 
-  static async register(data: RegisterUserDTO) {
-    const response = await fetch(`${this.USERS_URL}/registro`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+  static register = async (data: RegisterUserDTO) =>
+    basicFetch(`${this.USERS_URL}/registro`, "POST", data);
 
-    if (!response.ok) {
-      const { message } = (await response.json()) as TErrorResponse;
-      return {
-        success: false,
-        message,
-      };
-    }
-
-    return {
-      success: true,
-    };
-  }
-
-  static async login(
-    username: string,
-    password: string
-  ): Promise<TLoginResponse> {
-    const response = await fetch(`${this.USERS_URL}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
-
-    const loginOk = response.ok;
-    const data = await response.json();
-    if (!loginOk) {
-      const { message } = data as TErrorResponse;
-      return {
-        success: false,
-        message,
-      };
-    }
-
-    const user = data as TUser;
-    return {
-      success: true,
-      user,
-    };
-  }
+  static login = async (username: string, password: string) =>
+    basicFetch(
+      `${this.USERS_URL}/login`,
+      "POST",
+      { username, password },
+      (user: TUser) => ({
+        user,
+      })
+    );
 }
